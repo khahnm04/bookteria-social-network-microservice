7936fcb8bfbead87c3c8b2f37914918308013b56
khahnm04 <khanhnguyenminh1811@gmail.com>
2026-01-01 15:02:47 +0700

    Building api gateway with Spring Cloud Gateway

--- profile-service/src/main/java/com/devteria/profile/controller/InternalUserProfileController.java
+++ profile-service/src/main/java/com/devteria/profile/controller/InternalUserProfileController.java
@@ -1,22 +1,23 @@
 package com.devteria.profile.controller;
 
-import org.springframework.web.bind.annotation.*;
+import com.devteria.profile.dto.request.ProfileCreationRequest;
 import com.devteria.profile.dto.response.UserProfileResponse;
 import com.devteria.profile.service.UserProfileService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-public class UserProfileController {
+public class InternalUserProfileController {
 
     UserProfileService userProfileService;
 
-    @GetMapping("/users/{profileId}")
-    UserProfileResponse getProfile(@PathVariable String profileId) {
-        return userProfileService.getProfile(profileId);
+    @PostMapping("/internal/users")
+    UserProfileResponse createProfile(@RequestBody ProfileCreationRequest request) {
+        return userProfileService.createProfile(request);
     }
 
 }
