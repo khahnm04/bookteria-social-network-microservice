262b73814287ddcdb71744b879c250b61ee5c66e
khahnm04 <khanhnguyenminh1811@gmail.com>
2026-01-02 15:31:30 +0700

    Authentication with JWT for Api gateway

--- profile-service/src/main/java/com/devteria/profile/controller/UserProfileController.java
+++ profile-service/src/main/java/com/devteria/profile/controller/UserProfileController.java
@@ -6,6 +6,7 @@ import com.devteria.profile.service.UserProfileService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,6 +18,11 @@ public class UserProfileController {
     @GetMapping("/users/{profileId}")
     UserProfileResponse getProfile(@PathVariable String profileId) {
         return userProfileService.getProfile(profileId);
+    }
+
+    @GetMapping("/users")
+    List<UserProfileResponse> getAllProfiles() {
+        return userProfileService.getAllProfiles();
     }
 
 }
--- profile-service/src/main/java/com/devteria/profile/service/UserProfileService.java
+++ profile-service/src/main/java/com/devteria/profile/service/UserProfileService.java
@@ -7,11 +7,11 @@ import com.devteria.profile.dto.response.UserProfileResponse;
 import com.devteria.profile.entity.UserProfile;
 import com.devteria.profile.mapper.UserProfileMapper;
 import com.devteria.profile.repository.UserProfileRepository;
-
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -35,4 +35,11 @@ public class UserProfileService {
 
         return userProfileMapper.toUserProfileResponse(userProfile);
     }
+
+    public List<UserProfileResponse> getAllProfiles() {
+        var profiles = userProfileRepository.findAll();
+
+        return profiles.stream().map(userProfileMapper::toUserProfileResponse).toList();
+    }
+
 }
